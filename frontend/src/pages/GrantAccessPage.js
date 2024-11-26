90f33121d5914b292e2b6a98758e8fe495e35dc3
mrmrjing <hongjing123321@gmail.com>
2024-11-26 11:09:53 +0800

    Add grantaccesspage

---
+++ frontend/src/pages/GrantAccessPage.js
@@ -0,0 +1,13 @@
+// pages/GrantAccessPage.js
+import React from 'react';
+import GrantAccess from '../components/Patient/GrantAccessPage';
+
+const GrantAccessPage = () => {
+  return (
+    <div>
+      <GrantAccess />
+    </div>
+  );
+};
+
+export default GrantAccessPage;
